a120a8f19242610db7d3fb6fc997c4fdb6f0b2bd
davidsonmelo <davidspedrozamelo>
2022-02-14 21:24:21 -0300

    projeto portfoilio

---
+++ js/routes.js
@@ -0,0 +1,12 @@
+function mudaCEP() {
+  $("#content").load("/pages/busca-cep.html");
+  $(".sidenav-overlay").click();
+}
+function mudaCalc() {
+  $("#content").load("/pages/calculadora.html");
+  $(".sidenav-overlay").click();
+}
+function mudaPessoa() {
+  $("#content").load("/pages/pessoas.html");
+  $(".sidenav-overlay").click();
+}
